d62070cc97fe7cf597b62383e37510fb2a58c0fd
Chris Kubick <socalledsound@gmail.com>
2020-10-10 16:02:18 -0700

    constants

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 //Creating sprite using sprite sheets for animation
-
+const TILE_SIZE = 70;
+const CANVAS_WIDTH = TILE_SIZE * 9;
+const CANVAS_HEIGHT = TILE_SIZE * 6;
 let tile_sprite_sheet;
+
 
 function preload() {
   // Load the json for the tiles sprite sheet
@@ -11,9 +14,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(840, 420);
-
-
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
 function draw() {
@@ -21,16 +22,16 @@ function draw() {
   background(0);
 
   // Draw the ground tiles
-  for (var x = 0; x < 840; x += 70) {
-    tile_sprite_sheet.drawFrame('snow.png', x, 350);
+  for (var x = 0; x < CANVAS_WIDTH; x += TILE_SIZE) {
+    tile_sprite_sheet.drawFrame('snow.png', x, CANVAS_HEIGHT - TILE_SIZE);
   }
 
   // Draw the sign tiles
-  tile_sprite_sheet.drawFrame('signExit.png', 770, 280);
-  tile_sprite_sheet.drawFrame('signRight.png', 0, 280);
+  tile_sprite_sheet.drawFrame('signExit.png', CANVAS_WIDTH - TILE_SIZE, CANVAS_HEIGHT - TILE_SIZE * 2);
+  tile_sprite_sheet.drawFrame('signRight.png', 0, CANVAS_HEIGHT - TILE_SIZE * 2);
 
 
   //draw some more stuff
-  tile_sprite_sheet.drawFrame('boxCoin.png', 70, 70);
-  tile_sprite_sheet.drawFrame('boxCoinAlt.png', 140, 70);
+  tile_sprite_sheet.drawFrame('boxCoin.png', TILE_SIZE, TILE_SIZE);
+  tile_sprite_sheet.drawFrame('boxCoinAlt.png', TILE_SIZE * 2, TILE_SIZE);
 }
