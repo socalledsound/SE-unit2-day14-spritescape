ecaf97e60bc792196e1d7522db2ff963305945fd
Chris Kubick <socalledsound@gmail.com>
2020-10-10 17:23:13 -0700

    gridding done

--- sketch.js
+++ sketch.js
@@ -34,33 +34,64 @@ function setup() {
   // Draw the ground tiles
   for (var x = 0; x < TOTAL_COLUMNS; x++) {
     drawTile('snow.png', x, TOTAL_ROWS - 1);
-    rows[TOTAL_ROWS - 1][x] = 'snow.png';
+    // rows[TOTAL_ROWS - 1][x] = 'snow.png';
   }
 
   // Draw the sign tiles
   drawTile('signRight.png', 0, TOTAL_ROWS - 2);
-  rows[0][TOTAL_ROWS - 2] = 'signRight.png';
+  // rows[TOTAL_ROWS - 2][0] = 'signRight.png';
+  
   drawTile('signExit.png', 0, 0);
-  rows[0][0] = 'signExit.png';
+  // rows[0][0] = 'signExit.png';
+  
+
+
   //draw some more stuff
   drawTile('dirtCliffLeft.png', 1, 1);
+  // rows[1][1] = 'dirtCliffLeft.png';
+
   drawTile('dirtCliffRight.png', 2, 1);
+  // rows[1][2] = 'dirtCliffRight.png';
+
   // drawTile('boxCoinAlt.png', 3, 1);
   drawTile('boxCoinAlt.png', 4, 1);
+  // rows[1][4] = 'boxCoinAlt.png';
 
   drawTile('grassCliffLeft.png', 4, 3);
+  // rows[3][4] = 'grassCliffLeft.png';
+
   drawTile('grassCliffRight.png', 5, 3);
-  
+  // rows[3][5] = 'grassCliffRight.png';
+
   console.log(rows);
 }
 
 function draw() {
-  // clear();
+  clear();
   background(0);
   drawLevel();
-
 }
 
 function drawTile(tilename, gridX, gridY){
     tile_sprite_sheet.drawFrame(tilename, TILE_SIZE * gridX, TILE_SIZE * gridY);
+    rows[gridY][gridX] = tilename;
+}
+
+function drawLevel(){
+  rows.forEach((row, j) => {
+    row.forEach((item, i) => {
+      if(item != null){
+        drawTile(item, i, j);
+      }
+    })
+  })
+
+  // for(let i = 0; i < TOTAL_ROWS; i++){
+  //   for(let j = 0; j < TOTAL_COLUMNS; j++){
+  //     if(rows[i][j] != null){
+  //       drawTile(rows[i][j], j, i);
+  //     } 
+  //   }
+  // }
+
 }
